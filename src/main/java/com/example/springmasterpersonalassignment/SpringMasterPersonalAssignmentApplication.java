1a16dab3c284396f1fe9a07f94b66a8cbc1e245b
Kimhyeyun <cqqwer@gmail.com>
2023-11-18 13:46:55 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/example/springmasterpersonalassignment/SpringMasterPersonalAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmasterpersonalassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMasterPersonalAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMasterPersonalAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springmasterpersonalassignment/SpringMasterPersonalAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmasterpersonalassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMasterPersonalAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
