5311c3453ef34c78bdd8b945f0cd5a6f31480995
Kimhyeyun <cqqwer@gmail.com>
2023-11-19 16:00:35 +0900

    feat: jwt 토큰 생성, 토큰 확인 을 위한 필터 구현

---
+++ src/main/java/com/example/springmasterpersonalassignment/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,69 @@
+package com.example.springmasterpersonalassignment.jwt;
+
+import com.example.springmasterpersonalassignment.dto.LoginRequestDto;
+import com.example.springmasterpersonalassignment.security.UserDetailsImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Slf4j(topic = "로그인 및 JWT 생성")
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final JwtUtil jwtUtil;
+
+    public JwtAuthenticationFilter(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+        setFilterProcessesUrl("/api/user/login");
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        log.info("로그인 성공 및 JWT 생성");
+
+        String username = ((UserDetailsImpl) authResult.getPrincipal()).getUsername();
+
+        String token = jwtUtil.createToken(username);
+
+        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, token);
+        response.setStatus(HttpServletResponse.SC_OK);
+
+//        PrintWriter printWriter = response.getWriter();
+//        printWriter.println("로그인 성공 " + response.getStatus());
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        log.info("로그인 실패");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        log.info("로그인 시도");
+
+        try {
+            LoginRequestDto requestDto = new ObjectMapper().readValue(request.getInputStream(), LoginRequestDto.class);
+
+            return getAuthenticationManager().authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            requestDto.getUsername(),
+                            requestDto.getPassword()
+                    )
+            );
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+}
